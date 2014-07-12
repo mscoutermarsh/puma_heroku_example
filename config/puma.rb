8b5c61ac44e14ff3f19c20903c5aac72f01813f2
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2014-07-12 17:40:59 -0400

    Add a root page

--- config/puma.rb
+++ config/puma.rb
@@ -4,6 +4,7 @@ threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
 preload_app!
 
 rackup      DefaultRackup
+
 port = ENV['PORT'] || 3000
 backlog = ENV['PUMA_BACKLOG'] || 20
 
